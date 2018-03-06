b8fd3f920add4f1112726544ca1035df20f603b7
rayGreycloud <rayGreycloud@gmail.com>
2018-03-06 15:20:26 -0800

    Test withdrawal allowed by owner after lock date

--- test/TimeLockedWallet.test.js
+++ test/TimeLockedWallet.test.js
@@ -3,30 +3,23 @@ const TikiToken = artifacts.require("./TikiToken.sol");
 
 // Initialize variables 
 let ethToSend = web3.toWei(1, "ether");
-let someGas = web3.toWei(0.01, "ether");
-let amountOfTokens = 1000000000;
-let creator;
-let owner;
-let other; 
+let amountOfTokens = 1000;
+let creator = accounts[0];
+let owner = accounts[1];
+let other = accounts[2];
+const futureDate = Math.floor(new Date(2018, 12, 31) / 1000);
+const pastDate = Math.floor(new Date(2017, 12, 31) / 1000);
 
+// Helper function 
 async function createAndLoadWallet(creator, owner, unlockDate) {
-  let timeLockedWallet = await TimeLockedWallet.new(creator, owner, unlockDate);
+  let timeLockedWallet = await TimeLockedWallet.new(creator, owner, unlockDate); // Balance should be ethToSend - someGas
   await timeLockedWallet.send(ethToSend, {from: creator});
   let tikiToken = await TikiToken.new({from: creator});
   await tikiToken.transfer(timeLockedWallet.address, amountOfTokens, {from: creator});
 }
 
 contract('TimeLockedWallet', (accounts) => {
-  // Set up testing accounts before each test
-  before(async () => {
-    creator = accounts[0];
-    owner = accounts[1];
-    other = accounts[2];
-    
-    const futureDate = Math.floor(new Date(2018, 12, 31) / 1000);
-    const pastDate = Math.floor(new Date(2017, 12, 31) / 1000);
-  });
-  
+
   it("should not allow anyone to withdraw funds before unlock date", async () => {
     createAndLoadWallet(creator, owner, futureDate);
     
@@ -49,7 +42,16 @@ contract('TimeLockedWallet', (accounts) => {
   });
   
   it("should allow owner to withdraw funds after unlock date", async () => {
-    
+    // Create wallet with past unlock date 
+    createAndLoadWallet(creator, owner, pastDate);
+    // Get owner's starting balance
+    const startBalance = await web3.eth.getBalance(owner);
+    // Owner withdrawal 
+    await timeLockedWallet.withdraw({from: owner});
+    // Get ending balance 
+    const endingBalance = await web3.eth.getBalance(owner);
+    // Compare 
+    assert(endingBalance == startBalance + ethToSend));        
   });
   
   it("should not allow others to withdraw funds after unlock date", async () => {
